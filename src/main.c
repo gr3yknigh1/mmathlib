08dcd9f41e3dc255616057d1f55e358cc14237e2
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-10-18 21:20:24 +0300

    Update main

--- src/main.c
+++ src/main.c
@@ -3,43 +3,24 @@
 
 int main() {
 
-  // Matrix2 matrix = AllocateMatrix2(
-  //   (Vector2){ 4, 3 },
-  //   (double[]){
-  //     3, 7, 9, 4,
-  //     4, 0, 2, 1,
-  //     2, 3, 4, 0
-  //  }
-  // );
-  //
-  // PrintMatrix2(matrix);
-  //
-  // for (int y=0; y < matrix.shape.y; y++) {
-  //   for (int x=0; x < matrix.shape.x; x++) {
-  //     PrintMatrix2Element(matrix, x, y);
-  //     printf(" ");
-  //   }
-  //   printf("\n");
-  // }
-
-  Matrix2 matrix = AllocateMatrix2(
-    (Vector2){2, 2},
-    malloc(sizeof(double) * 4)
+  Matrix2 matrix = CreateMatrix2(
+    (Vector2){ 4, 3 },
+    (double[]){
+      3, 7, 9, 4,
+      4, 0, 2, 1,
+      2, 3, 4, 0
+   }
   );
 
-  matrix.data[0] = 1;
-  printf("%lf\n", matrix.data[0]);
-  free(matrix.data);
-  printf("%lf\n", matrix.data[0]);
+  PrintMatrix2(matrix);
 
-  // Matrix2 matrix = AllocateMatrix2(
-  //   (Vector2){2, 2},
-  //   (double[]) {1, 2, 3, 4}
-  // );
-  // matrix.data[0] = 1;
-  // printf("%lf\n", matrix.data[0]);
-  // free(matrix.data);
-  // printf("%lf\n", matrix.data[0]);
+  for (int y=0; y < matrix.shape.y; y++) {
+    for (int x=0; x < matrix.shape.x; x++) {
+      PrintMatrix2Element(matrix, x, y);
+      printf(" ");
+    }
+    printf("\n");
+  }
 
   return 0;
 }
