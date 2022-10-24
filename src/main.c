5389b49c999500f6204e1d3d2fa84642f870c445
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-10-24 14:15:54 +0300

    Reorg main

--- src/main.c
+++ src/main.c
@@ -1,31 +1,10 @@
 #include <stdio.h>
-#include "mmath.h"
+#include <stdbool.h>
 
-
-#define true  1
-#define false 0
-#define OK    0
-#define ERR   1
+#include <mmath.h>
 
 
 int main(void) {
-
-  LinkedList list = {
-    .root = (LinkedListItem*)0,
-    .length = 0,
-  };
-
-  const int ARR_SIZE = 8;
-  int* arr = malloc(sizeof(int) * ARR_SIZE);
-  arr = (int[]){ 1, 2, 3, 4, 4, 4, 5, 9 };
-
-  for (int i = 0; i < ARR_SIZE; i++) {
-    PushItem(&list, &arr[i], sizeof(int));
-  }
-
-  printf("Length: %d\n", list.length);
-
-  PrintList(&list, Int2String, ", ");
 
   return OK;
 }
